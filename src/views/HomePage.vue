387b19d5b445c08cd8ba0382da47f9239c0327d2
MRSN37 <60518972+MRSN37@users.noreply.github.com>
2022-11-30 11:46:11 +0700

    UPDATE: Implementasi Frontend Homepage SIB-1
    - Fix all margin and padding 
    - Change all fonts to Poppins as in mock up
    - Fix responsiveness at background gradient color

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -2,7 +2,7 @@
   <ion-page>
     <ion-content :fullscreen="true">
       <ion-toolbar style="color:#343A40">
-        <ion-title style="color:white">Kampung<span style="color:#00DC72">Siber</span></ion-title>
+        <ion-title style="color:white; margin-left :20px">Kampung<span style="color:#00DC72">Siber</span></ion-title>
         <ion-buttons class ="button-nav" slot="primary">
           <ion-button style="padding-left:10px">
             Beranda
@@ -21,57 +21,32 @@
 
       <ion-grid>
   <ion-row>
-    <ion-col>
-      
-    </ion-col>
+
     <ion-col class="back1">
       <span class="title1">Kampung<span style="color:#00DC72">Siber</span></span>
+      <span class="text1">Klinik Coding Pertama di Indonesia!</span>
+      <span class="text2"><span class="text2.a">Sekarang kamu ga perlu overthinking menghadapi <br>error dan bug lagi dengan konsultasi sama "dokter-dokter"</span><br>coding kita sesuai spesialisasinya lho</span>
+      <div class="box1"></div>
+      <ion-img id="img1" src="https://i.postimg.cc/8kKqbpjg/Group-17.png" alt="pict1"></ion-img>
+      <ion-img id="img2" src="https://i.postimg.cc/44jq78Yz/Group-18.png" alt="pict1"></ion-img>
+      <ion-button id="btn1" color="success" size="large" ><span class=btn1text>Book Sekarang!!!</span></ion-button>
+      
     </ion-col>
-  </ion-row>
+    </ion-row>
+      <ion-row>
+    <ion-col>
+      <span class="text3">Mengapa kita lebih <br>baik dari yang lain</span>
+      <span class="text4">Tentu jika kamu ingin memilih kami 
+<br>maka kamu harus tau alasan untuk
+<br>memilih kami kan? 
+<br>nah ini dia alasannya!</span>
+    </ion-col>
+    <ion-col>
+  
+    </ion-col>
+    </ion-row>
 </ion-grid>
-      <!-- <ion-grid class="first">
-          <ion-img src="https://cdn.discordapp.com/attachments/908419630467530823/1043603725970837594/unknown.png" alt="The Wisconsin State Capitol building in Madison, WI at night"></ion-img>
-      </ion-grid>
-      <div>
-        <h3 style="color:black;text-align: center;">Pembelajaran  bahasa pemrograman yang tersedia</h3>
-      </div> -->
 
-      <!-- <nav class="navbar navbar-expand-lg bg-light">
-  <div class="container-fluid">
-    <a class="navbar-brand" href="#">Navbar</a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-        <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="#">Home</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Link</a>
-        </li>
-        <li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            Dropdown
-          </a>
-          <ul class="dropdown-menu">
-            <li><a class="dropdown-item" href="#">Action</a></li>
-            <li><a class="dropdown-item" href="#">Another action</a></li>
-            <li><hr class="dropdown-divider"></li>
-            <li><a class="dropdown-item" href="#">Something else here</a></li>
-          </ul>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link disabled">Disabled</a>
-        </li>
-      </ul>
-      <form class="d-flex" role="search">
-        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-        <button class="btn btn-outline-success" type="submit">Search</button>
-      </form>
-    </div>
-  </div>
-</nav> -->
     </ion-content>
   </ion-page>
 </template>
@@ -80,16 +55,22 @@
 import { IonContent, IonPage,IonToolbar, IonTitle, IonButton, IonButtons ,IonGrid,IonCol,IonRow} from '@ionic/vue';
 import { defineComponent } from 'vue';
 
+
 export default defineComponent({
   name: 'HomePage',
   components: {
     IonContent,
-    IonPage,IonToolbar, IonTitle, IonButton, IonButtons,IonGrid,IonCol,IonRow 
+    IonPage,IonToolbar, IonTitle, IonButton, IonButtons,IonGrid,IonCol,IonRow,
   }
 });
 </script>
 
 <style scoped>
+*{
+  margin : 0;
+  padding : 0;
+  font-family: "Poppins"
+}
 #container {
   text-align: center;
   
@@ -140,11 +121,137 @@ color: #FFFFFF;
 }
 .back1{
   position: absolute;
-width: 1920px;
+width: 100%;
 height: 840px;
 left: 0px;
-
-
 background: linear-gradient(180deg, #000D18 0%, #021C35 100%);
 }
+
+.text1{
+  position: absolute;
+width: 849px;
+height: 68px;
+left: 19px;
+top: 210px;
+
+font-family: 'Poppins';
+font-style: normal;
+font-weight: 600;
+font-size: 45px;
+line-height: 68px;
+/* identical to box height */
+color: #FFFFFF;
+}
+
+.text2{
+  position: absolute;
+width: 1826px;
+height: 212px;
+left: 22px;
+top: 274px;
+
+font-family: 'Poppins';
+font-style: normal;
+font-weight: 400;
+font-size: 35px;
+line-height: 52px;
+
+color: #FFFFFF;
+}
+#img1{
+  box-sizing: border-box;
+
+position: absolute;
+width: 555px;
+height: 469.33px;
+left: 1138px;
+top: 47.67px;
+
+background: #02203C;
+border: 3px solid #333564;
+box-shadow: inset 0px 4px 4px rgba(0, 0, 0, 0.4);
+border-radius: 10px;
+}
+.text2.a{
+  left: 42px;
+}
+
+#img2{
+  box-sizing: border-box;
+
+position: absolute;
+width: 555px;
+height: 469.33px;
+left: 1353px;
+top: 291px;
+
+background: #02203C;
+border: 3px solid #333564;
+box-shadow: inset 0px 4px 4px rgba(0, 0, 0, 0.4);
+border-radius: 10px;
+}
+
+#btn1{
+  position: absolute;
+width: 397px;
+height: 70px;
+left: 57px;
+top: 490px;
+}
+
+.btn1text{
+  font: "Poppins";
+  font-size: 35px;
+  font-weight: bold;
+  color: rgb(107, 107, 107);
+}
+
+.text3{
+  position: absolute;
+width: 553px;
+height: 166px;
+left: 55px;
+top: 968px;
+
+font-family: 'Poppins';
+font-style: normal;
+font-weight: 600;
+font-size: 55px;
+line-height: 82px;
+
+color: #000000;
+
+}
+
+.text4{
+  position: absolute;
+width: 628px;
+height: 212px;
+left: 55px;
+top: 1157px;
+
+font-family: 'Poppins';
+font-style: normal;
+font-weight: 400;
+font-size: 35px;
+line-height: 52px;
+
+color: #000000;
+}
+
+.card1{
+  position: absolute;
+width: 298px;
+height: 152px;
+left: 739px;
+top: 1301px;
+
+font-family: 'Poppins';
+font-style: normal;
+font-weight: 600;
+font-size: 25px;
+line-height: 38px;
+text-align: center;
+}
+
 </style>
