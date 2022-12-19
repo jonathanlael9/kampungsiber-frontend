2527e29414a33a833d4d168bc2a65307052b4c41
baccaratt <firmanaprila46@gmail.com>
2022-12-19 23:43:01 +0700

    ADD: Implementasi frontend Dashboard - SIB - 2 UPDATE: Update consistent style for all pages - SIB - 6

---
+++ src/views/components/Mentor.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="border border-danger mentor">
+        <img src="../../../public/assets/icon/placeholder_user.png" height="120" width="120">
+        <h5 class="text-dark">Ujang Saepuloh</h5>
+    </div>
+</template>
+<style scoped>
+    .mentor{
+        width: 200px;
+        height: 200px;
+    }
+</style>
+<script lang="ts">
+    import { defineComponent } from 'vue';
+    export default defineComponent({
+        name : "MentorVue"
+    })
+</script>
