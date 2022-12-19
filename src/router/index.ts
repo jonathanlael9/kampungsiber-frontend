74fac3e404e8af07450cfcca5926c67348bbc974
Jonathan lael aruan <114045843+jonathanlael9@users.noreply.github.com>
2022-12-19 17:50:57 +0700

    Update index.ts

--- src/router/index.ts
+++ src/router/index.ts
@@ -3,12 +3,6 @@ import { RouteRecordRaw } from 'vue-router';
 import HomePage from '../views/HomePage.vue'
 import DashboardPage from '../views/Dashboard.vue'
 import BookingPage from '../views/Booking.vue';
-import SelectMentorVue from '@/views/SelectMentor.vue';
-import BookingInfoVue from '@/views/BookingInfo.vue';
-import PaymentMethod from '@/views/PaymentMethod.vue'
-import PaymentPage from '@/views/PaymentPage.vue';
-import SuccessPageVue from '@/views/SuccessPage.vue';
-
 const routes: Array<RouteRecordRaw> = [
   {
     path: '/',
@@ -29,31 +23,6 @@ const routes: Array<RouteRecordRaw> = [
     name: 'BookingMentor',
     component: BookingPage
   },
-  {
-    path: '/booking-consultation/:id',
-    name: 'SelectMentor',
-    component : SelectMentorVue
-  },
-  {
-    path: '/booking-consultation/:id/info',
-    name: 'BookingInfo',
-    component: BookingInfoVue
-  },
-  {
-    path: '/booking-consultation/payment',
-    name: 'PaymentMethod',
-    component : PaymentMethod
-  },
-  {
-    path: '/booking-consultation/payment/detail',
-    name: 'PaymentPage',
-    component : PaymentPage
-  },
-  {
-    path: '/booking-consultation/payment/success',
-    name: 'SuccessPage',
-    component : SuccessPageVue
-  }
 ]
 
 const router = createRouter({
